00021ae4a95bf951638b3e08ebbb4fc057100945
cjshieh <chingjshieh@gmail.com>
2017-10-29 16:15:23 -0700

    Create init method for event listener

--- Budgety/app.js
+++ Budgety/app.js
@@ -9,7 +9,7 @@ var UIController = (function () {
     inputValue: '.add__value',
     inputButton:'.add__btn'
   };
-  
+
   return {
     getInput: function () {
       return {
@@ -25,22 +25,35 @@ var UIController = (function () {
 })();
 
 var controller = (function (budgetCtrl, UICtrl) {
-  var DOM = UICtrl.getDOMstrings();
+
+  var setupEventListner = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+
+  }
+
   var ctrlAddItem = function () {
     // 1. Get the field input data
     var input = UICtrl.getInput();
-    console.log(input);
+    // console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-  }
+  };
 
-  document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-  document.addEventListener('keypress', function (event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: function() {
+      setupEventListner();
     }
-  })
+  };
 
 })(budgetController, UIController);
+
+controller.init();
