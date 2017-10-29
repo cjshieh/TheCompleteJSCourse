2c8991cdba4a139e5929a4641d72c973c6c7039e
cjshieh <chingjshieh@gmail.com>
2017-10-29 16:05:06 -0700

    Create getter in UIController

--- Budgety/app.js
+++ Budgety/app.js
@@ -1,23 +1,44 @@
-var budgetController = (function() {
+var budgetController = (function () {
 
 })();
 
-var UIController = (function() {
-
+var UIController = (function () {
+  var DOMSTRINGS = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputButton:'.add__btn'
+  };
+  
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMSTRINGS.inputType).value, //will be either inc or exp
+        description: document.querySelector(DOMSTRINGS.inputDescription).value,
+        value: document.querySelector(DOMSTRINGS.inputValue).value
+      }
+    },
+    getDOMstrings: function() {
+      return DOMSTRINGS;
+    }
+  }
 })();
 
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
   var ctrlAddItem = function () {
     // 1. Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
   }
 
-  document.querySelector('.add_btn').addEventListener('click', ctrlAddItem);
-  document.addEventListener('keypress', function(event) {
-    if(event.keyCode === 13 || event.which === 13) {
+  document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+  document.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
     }
   })
