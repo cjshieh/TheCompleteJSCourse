c6df20098f45f57ef788fd9ad9c048750ec1917d
cjshieh <chingjshieh@gmail.com>
2017-10-29 15:39:34 -0700

    Create global window controller

---
+++ Budgety/app.js
@@ -0,0 +1,25 @@
+var budgetController = (function() {
+
+})();
+
+var UIController = (function() {
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+  var ctrlAddItem = function () {
+    // 1. Get the field input data
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+  }
+
+  document.querySelector('.add_btn').addEventListener('click', ctrlAddItem);
+  document.addEventListener('keypress', function(event) {
+    if(event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  })
+
+})(budgetController, UIController);
